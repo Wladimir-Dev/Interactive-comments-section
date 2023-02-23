1fab9107c86c19740556e30e60639526eea08951
wladimir-dev <wladimir.gonzalez18@gmail.com>
2023-02-23 09:19:23 -0400

    improved prepareContent

--- src/utilities/prepareContent.js
+++ src/utilities/prepareContent.js
@@ -3,7 +3,12 @@ import React from 'react'
 export function prepareContent(textContent) {
 
     const text = textContent.split(" ");
-    text.splice(0, 1);
+    const str1 = text[0];
+
+    if (str1.startsWith("@")) {
+        text.splice(0, 1);
+    }
+    
     return (text.join(' '))
-  
+
 }
